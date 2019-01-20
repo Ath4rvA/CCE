2f3a4f8c115c01efc9d6239718aa4389da944312
Ath4rvA <30554295+Ath4rvA@users.noreply.github.com>
2019-01-20 21:48:29 +0530

    Add files via upload

--- fetch_try.py
+++ fetch_try.py
@@ -3,6 +3,7 @@ import json
 sys.path.append("C:/Users/Atharva/Desktop/My stuff/BE PROJ/cce try/try/")
 from youtube_videos import youtube_search
 import json
+import pandas as pd
 x=input("enter search term here: ")
 results=youtube_search(x)
 #print(results)
@@ -12,6 +13,12 @@ just_json=results[1]
 dump_list=json.dumps(just_json)
 parsed=json.loads(dump_list)
 print(json.dumps(parsed, indent=4, sort_keys=True))
+
+raw_df=[]
+for data in just_json:
+    raw_df.append(data['id']['videoId'])
+df=pd.DataFrame(raw_df,columns=['VideoId'])
+df.to_csv('videoid.csv')
 #for video in just_json:
 	#print (video['snippet']['title'])
 
